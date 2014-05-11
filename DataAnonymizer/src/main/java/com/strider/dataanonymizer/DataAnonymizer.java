589d264453589b72688eb7a3c075fa73cac7a6ae
Armenak Grigoryan <armenak@mail.ubc.ca>
2014-05-11 22:15:46 +0200

    Initial check-in.

---
+++ DataAnonymizer/src/main/java/com/strider/dataanonymizer/DataAnonymizer.java
@@ -0,0 +1,16 @@
+package com.strider.dataanonymizer;
+
+/**
+ * Entry point to Data Anonymizer. 
+ * 
+ * This class will parse and analyze the parameters and execute appropriate 
+ * service.
+ *
+ */
+public class DataAnonymizer 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Start" );
+    }
+}
