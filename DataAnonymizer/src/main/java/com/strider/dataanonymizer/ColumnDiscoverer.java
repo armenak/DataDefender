1f45b40cf41834a4e9fba846a795b4ce99b75603
Armenak Grigoryan <armenak@mail.ubc.ca>
2014-05-15 23:31:39 +0200

    Rearranging the code.

--- DataAnonymizer/src/main/java/com/strider/dataanonymizer/ColumnDiscoverer.java
+++ DataAnonymizer/src/main/java/com/strider/dataanonymizer/ColumnDiscoverer.java
@@ -5,6 +5,20 @@ import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.util.Properties;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import com.strider.dataanonymizer.utils.AppProperties;
+import java.util.Iterator;
+
 /**
  *
  * @author strider
@@ -15,20 +29,18 @@ public class ColumnDiscoverer implements IDiscoverer {
         
         System.out.println("Field discoverer");
         // Get the metadata from the the database
+        SortedMap map = new TreeMap();
         try {
             // Getting all tables name
             DatabaseMetaData md = conn.getMetaData();
             ResultSet rs = md.getTables(null, null, "%", null);
-            System.out.println("List of tables in database");
             while (rs.next()) {
                 String tableName = rs.getString(3);
-                System.out.println(tableName);
                 ResultSet resultSet = md.getColumns(null, null, tableName, null);        
                 while (resultSet.next()) {
-                    String name = resultSet.getString("COLUMN_NAME");
-                    String type = resultSet.getString("TYPE_NAME");
-                    int size = resultSet.getInt("COLUMN_SIZE");
-                    System.out.println("Column name: [" + name + "]; type: [" + type + "]; size: [" + size + "]");
+                    String columnName = resultSet.getString("COLUMN_NAME");
+                    map.put(tableName, columnName);
+                    System.out.println("table:"+tableName+" column:"+columnName);
                 }
             }
         } catch (SQLException e) {
@@ -37,8 +49,33 @@ public class ColumnDiscoverer implements IDiscoverer {
         
         
         // Get the list of "suspicios" field names from property file
+        InputStream input = null;
+        try{
+            input = AppProperties.class.getClassLoader().getResourceAsStream("fields.properties");
+            // Get the object of DataInputStream
+            DataInputStream in = new DataInputStream(input);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String strLine;
+            //Read File Line By Line
+            while ((strLine = br.readLine()) != null)   {
+                // Print the content on the console
+                System.out.println (strLine);
+            }
+            //Close the input stream
+            in.close();
+        }catch (Exception e){//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+       
+        Iterator iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            Object key = iterator.next();
+            System.out.println("key : " + key + " value :" + map.get(key));
+        }
         
         // Find out if database columns contain any of of the "suspicios" fields
+
+        
         
         // Report column names
         
