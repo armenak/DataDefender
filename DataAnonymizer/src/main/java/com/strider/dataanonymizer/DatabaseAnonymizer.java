e394f8d44bd180d0e482645bbb0f7a1d1a482845
Zaahid Bateson <zbateson@mail.ubc.ca>
2014-12-17 11:24:32 -0800

    Better handling of NULL values
    - Handles NULL values for empty columns a little better.
    - Moved index icrement to the top so it doesn't need to be
      incremented before each 'continue', etc...

--- DataAnonymizer/src/main/java/com/strider/dataanonymizer/DatabaseAnonymizer.java
+++ DataAnonymizer/src/main/java/com/strider/dataanonymizer/DatabaseAnonymizer.java
@@ -37,6 +37,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.Properties;
 import java.util.logging.Level;
 import javax.xml.bind.JAXBContext;
@@ -129,22 +130,22 @@ public class DatabaseAnonymizer implements IAnonymizer {
                     int index = 0;
                     
                     for(Column column : table.getColumns()) {
-                    
+
+                        ++index;
+
                         if (column.isIgnoreEmpty()) {
                             String colName = column.getName();
                             Statement cStmt = connection.createStatement();
                             ResultSet cRs = cStmt.executeQuery(String.format("SELECT %s FROM %s WHERE %s = %d", colName, table.getName(), table.getPKey(), id));
-                            if (!cRs.next()) {
-                                pstmt.setString(++index, "");
-                                continue;
+                            if (cRs.next()) {
+                                String value = cRs.getString(colName);
+                                cStmt.close();
+                                cRs.close();
+                                if (value == null || value.length() == 0) {
+                                    pstmt.setString(index, value);
+                                    continue;
+                                }
                             }
-                            String value = cRs.getString(colName);
-                            if (value == null || value.length() == 0) {
-                                pstmt.setString(++index, "");
-                                continue;
-                            }
-                            cStmt.close();
-                            cRs.close();
                         }
                     
                         String function = column.getFunction();
@@ -160,7 +161,7 @@ public class DatabaseAnonymizer implements IAnonymizer {
 
                                 if (column.getParameters() == null) {
                                     Method method = clazz.getMethod(methodName,null);
-                                    pstmt.setString(++index, method.invoke(instance).toString());
+                                    pstmt.setString(index, method.invoke(instance).toString());
                                 } else {
                                     Method method = clazz.getMethod(methodName, String[].class);
                                     String[] stringParams = new String[column.getParameters().size()];
@@ -170,7 +171,7 @@ public class DatabaseAnonymizer implements IAnonymizer {
                                     }
                                     //Object result = method.invoke(null, (Object)stringParams);
                                     Object result = method.invoke(instance, (Object)stringParams);
-                                    pstmt.setString(++index, result.toString());
+                                    pstmt.setString(index, result.toString());
                                 }
                             } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) {
                                 log.error(ex.toString());
