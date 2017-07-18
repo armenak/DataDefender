35732bd3cd39ff01d08df713916bf81270d62fec
Luís Marques <lcarapinha@gmail.com>
2017-07-18 16:03:45 +0100

    Update FileDiscoverer.java
    Some errors were made in merge

--- src/main/java/com/strider/datadefender/FileDiscoverer.java
+++ src/main/java/com/strider/datadefender/FileDiscoverer.java
@@ -132,11 +132,7 @@ public class FileDiscoverer extends Discoverer {
                     metadata = new Metadata();
                     String handlerString = "";
                     try  {
-<<<<<<< HEAD
                     final InputStream stream = new FileInputStream(directory + "/" + file);
-=======
-                    InputStream stream = new FileInputStream(directory + "/" + file);
->>>>>>> MSSQLLimitFix
                         if (stream != null) {
                             parser.parse(stream, handler, metadata);
                             handlerString =  handler.toString();
@@ -144,15 +140,7 @@ public class FileDiscoverer extends Discoverer {
                     }
                     catch (IOException e) {
                       log.info("Unable to read " + directory + "/" + file + ". Ignoring...");
-<<<<<<< HEAD
                       }
-=======
-<<<<<<< HEAD
-                      } 
-=======
-                      }
->>>>>>> FileDiscoverySubdirectories
->>>>>>> MSSQLLimitFix
 
                     log.debug("Content: " + handlerString);
                     final String tokens[] = model.getTokenizer().tokenize(handler.toString());
