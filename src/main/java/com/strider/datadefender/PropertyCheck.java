9fe507a7dcb2a53b91f695b8649ac5c54f88ca93
Luís Marques <lcarapinha@gmail.com>
2017-07-21 19:43:47 +0100

    Added Recursive File Discovery against directories specified in directories parameter
    Decided to implement Recursive file discovery against the directories
    specified in the parameter in filediscovery.properties. If you have a
    repository (windows shares) with huge amounts of directories and
    subdirectories, it is much faster to do it automatically.
    Also cleaned some output.
    Please review if that feature makes sense for you.

--- src/main/java/com/strider/datadefender/PropertyCheck.java
+++ src/main/java/com/strider/datadefender/PropertyCheck.java
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  * Copyright 2014-2016, Armenak Grigoryan, and individual contributors as indicated
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
@@ -23,19 +23,19 @@ import java.util.List;
 import java.util.Properties;
 
 /**
- * Check for properties required for execution of data defender and data anonymizer 
+ * Check for properties required for execution of data defender and data anonymizer
  * and stop the execution if not all required properties are defined
  * @author Armenak Grigoryan
  */
 public class PropertyCheck {
-    
+
     private static final String[] fileDiscoveryProperties = {"probability_threshold", "english_tokens", "english_sentenses", "names", "models", "directories"};
-    private static final String[] dataDiscoveryProperties = {"probability_threshold", "english_tokens", "names", "models"};
-    private static final String[] databaseProperties = {"vendor", "driver", "username", "password", "url"};    
+    private static final String[] dataDiscoveryProperties = {"probability_threshold", "english_tokens", "names", "models", "limit", "extentions", "score_calculation", "threshold_count", "threshold_highrisk"};
+    private static final String[] databaseProperties = {"vendor", "driver", "username", "password", "url"};
     private static final String[] dataAnonymizerProperties = {"requirement", "batch_size"};
-   
+
     @SuppressWarnings("unchecked")
-    
+
     public static List checkDtabaseProperties() throws DataDefenderException {
         final List<String> errors = new ArrayList<>();
 
@@ -46,17 +46,17 @@ public class PropertyCheck {
             if (property == null || "".equals(property)) {
                 errors.add("Property " + databaseProperties[i] + " is not defined");
             }
-        }       
+        }
         return errors;
     }
-    
+
     @SuppressWarnings("unchecked")
     public static List check(final String utiity, final char option) throws DataDefenderException {
-        
+
         final List errors = new ArrayList<>();
         if ("file-discovery".equals(utiity)) {
             final Properties properties = loadProperties("filediscovery.properties");
-            
+
             for (int i=0; i<fileDiscoveryProperties.length; i++ ) {
                 final String property = properties.getProperty(fileDiscoveryProperties[i]);
                 if (property == null || "".equals(property)) {
@@ -65,7 +65,7 @@ public class PropertyCheck {
             }
         } else if ("anonymize".equals(utiity)) {
             final Properties properties = loadProperties("anonymizer.properties");
-            
+
             for (int i=0; i<dataAnonymizerProperties.length; i++ ) {
                 final String property = properties.getProperty(dataAnonymizerProperties[i]);
                 if (property == null || "".equals(property)) {
@@ -86,7 +86,7 @@ public class PropertyCheck {
                     if (property == null || "".equals(property)) {
                         errors.add("Property " + dataDiscoveryProperties[i] + " is not defined");
                     }
-                }                
+                }
             }
         }
         return errors;
