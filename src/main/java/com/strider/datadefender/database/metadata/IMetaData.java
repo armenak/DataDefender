66d4240d475ae0b54248b2a5cb73807a4c730e5b
Zaahid Bateson <zbateson@users.noreply.github.com>
2020-03-13 12:27:51 -0700

    Refactor getMetaData to use DbConfig.Vendor

--- src/main/java/com/strider/datadefender/database/metadata/IMetaData.java
+++ src/main/java/com/strider/datadefender/database/metadata/IMetaData.java
@@ -1,10 +1,25 @@
+/*
+ * Copyright 2014, Armenak Grigoryan, and individual contributors as indicated
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
 package com.strider.datadefender.database.metadata;
+
+import com.strider.datadefender.DbConfig.Vendor;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import java.util.List;
-import java.util.Set;
 
 /**
  * Public interface to define the contract for all database-specific
@@ -13,8 +28,6 @@ import java.util.Set;
  * @author armenak
  */
 public interface IMetaData {
-    List<MatchMetaData> getMetaData(String vendor);
-
-    // List<MatchMetaData> getMetaData(final String columnType);
+    List<MatchMetaData> getMetaData(Vendor vendor);
     List<MatchMetaData> getMetaDataForRs(final ResultSet rs) throws SQLException;
 }
