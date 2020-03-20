6fbd89cbbfbb5cb0a597255c245ae9ea2654d7f6
Zaahid Bateson <zbateson@users.noreply.github.com>
2020-03-20 12:19:41 -0700

    Fix UtilsTest

--- src/test/java/com/strider/datadefender/functions/UtilsTest.java
+++ src/test/java/com/strider/datadefender/functions/UtilsTest.java
@@ -1,5 +1,4 @@
 /*
- *
  * Copyright 2014, Armenak Grigoryan, and individual contributors as indicated
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
@@ -13,79 +12,37 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
  */
-
-
-
 package com.strider.datadefender.functions;
-
-import org.apache.log4j.Logger;
-
-import org.junit.Test;
-
-import static org.apache.log4j.Logger.getLogger;
 
 import com.strider.datadefender.anonymizer.DatabaseAnonymizerException;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Armenak Grigoryan
  */
-public class UtilsTest extends TestCase {
+public class UtilsTest {
 
     /**
      * Initializes logger
      */
-    private static final Logger log                = getLogger(UtilsTest.class);
-    private static final String FULL_CLASS_NAME    = "com.strider.datadefender.functions.CoreFunctions";
-    private static final String EXPECTED_RESULTS_1 = "com.strider.datadefender.functions";
-    private static final String EXPECTED_RESULTS_2 = "CoreFunctions";
+    private final String FULL_CLASS_NAME    = "com.strider.datadefender.anonymizer.functions.Core";
+    private final String EXPECTED_RESULTS_1 = "com.strider.datadefender.functions.functions";
+    private final String EXPECTED_RESULTS_2 = "Core";
 
-    public UtilsTest(final String testName) {
-        super(testName);
-    }
-
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();
-    }
-
-    /**
-     * Test of getClassName method, of class Utils.
-     * @throws com.strider.datadefender.anonymizer.DatabaseAnonymizerException
-     */
     @Test
-    public void testGetClassName() throws DatabaseAnonymizerException {
-        log.info("Executing testGetClassName ...");
-        log.debug("Parameter: " + FULL_CLASS_NAME);
-        log.debug("Expected result:" + EXPECTED_RESULTS_1);
-
+    public void testGetClassName() throws Exception {
+        System.out.println("Executing testGetClassName...");
         final String result = Utils.getClassName(FULL_CLASS_NAME);
-
         assertEquals(EXPECTED_RESULTS_1, result);
     }
 
-    /**
-     * Test of getMethodName method, of class Utils.
-     * @throws com.strider.datadefender.anonymizer.DatabaseAnonymizerException
-     */
     @Test
     public void testGetMethodName() throws DatabaseAnonymizerException {
-        log.debug("Parameter: " + FULL_CLASS_NAME);
-        log.debug("Expected result:" + EXPECTED_RESULTS_2);
-
+        System.out.println("Executing testGetMethodName...");
         final String result = Utils.getMethodName(FULL_CLASS_NAME);
-
         assertEquals(EXPECTED_RESULTS_2, result);
     }
-
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
 }
-
-
-//~ Formatted by Jindent --- http://www.jindent.com
